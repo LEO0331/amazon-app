946300144198bc3942b0df009a6c8967a2c197dc
Li-Cheng Chen <li-cheng.chen@student.adelaide.edu.au>
2021-08-29 15:10:21 +0930

    rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import data from "./data";
-
+import Products from "./components/Products"
 
 function App() {
   return (
@@ -18,25 +18,8 @@ function App() {
         <div>
           <div className="row center">
             {
-              data.products.map(p => (
-                <div key={p._id} className="card">
-                  <a href={`/product/{p._id}`}>
-                    <img className="medium" src={p.image} alt={p.name}/>
-                  </a>
-                  <div className="card-body">
-                    <a href={`/product/{p._id}`}>
-                      <h2>{p.name}</h2>
-                    </a>
-                    <div className="rating">
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                      <span> <i className="fa fa-star"></i> </span>
-                    </div>
-                    <div className="price">{p.price}</div>
-                  </div>
-                </div>
+              data.products.map(product => (
+                <Products key={product._id} product={product}/>
               ))
             }
           </div>
