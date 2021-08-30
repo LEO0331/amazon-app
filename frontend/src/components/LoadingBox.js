a9c0b77f4796098be4510bb7f2358eb1765804a8
Li-Cheng Chen <li-cheng.chen@student.adelaide.edu.au>
2021-08-31 00:19:44 +0930

    two screens and Hook

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//https://fontawesome.com/v5.15/how-to-use/on-the-web/styling/animating-icons
+function LoadingBox() {
+  return (
+    <div className="loading">
+      <i className="fa fa-spinner fa-pulse"></i> Loading...
+    </div>
+  );
+}
+
+export default LoadingBox;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,19 +1,51 @@
-import React from 'react'
-import data from "../data";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Products from "../components/Products";
-//params => ({foo: "a"}) // returning the object {foo: "a"}
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from '../components/MessageBox';
+
+//params => ({foo: "a"}); returning the object {foo: "a"}
 function HomeScreen() {
-    return (
-      <div>
-        <div className="row center">
+  //https://reactjs.org/docs/hooks-intro.html
+  const [products, setProducts] = useState([]); //use state and other React features without writing a class
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  // Similar to componentDidMount and componentDidUpdate: Data fetching, setting up a subscription, and manually changing the DOM in React components are all examples of side effects
+  useEffect(() => {
+    // send ajax to backend and fetch products
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const {data} = await axios.get('/api/products'); //backend array transfer to frontend
+        setLoading(false);
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      {
+        loading ? (
+        <LoadingBox />
+        ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <div className="row center">
           {
-            data.products.map(product => (
+            products.map(product => (
               <Products key={product._id} product={product} />
             ))
           }
-        </div>
-      </div>
-    )
+          </div>
+        )
+      }
+
+    </div>
+  )
 }
 
 export default HomeScreen;
